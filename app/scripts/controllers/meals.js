928b1f6c7747099192b117ef1e1181638522aab7
Francesco Fiori <francesco.fiori@gmail.com>
2016-03-13 10:50:04 +0100

    refactoring mealsctrl

--- app/scripts/controllers/meals.js
+++ app/scripts/controllers/meals.js
@@ -1,16 +1,32 @@
-'use strict';
+(function () {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name jstestApp.controller:MealsCtrl
- * @description
- * # MealsCtrl
- * Controller of the jstestApp
- */
-angular.module('jstestApp')
-  .controller('MealsCtrl', ['$scope', function ($scope) {
-    $scope.addToCart = function(mealId) {
+  /**
+   * @ngdoc function
+   * @name jstestApp.controller:MealsCtrl
+   * @description
+   * # MealsCtrl
+   * Controller for meal list
+   */
+  angular
+    .module('jstestApp')
+    .controller('MealsCtrl', MealsCtrl);
+
+  MealsCtrl.$inject = ['$scope'];
+  /* @ngInject */
+  function MealsCtrl($scope) {
+    var vm = this;
+
+    activate();
+
+    vm.addToCart = addToCart;
+
+    function activate() {
+      console.log('ciao');
+    }
+
+    function addToCart (mealId) {
       console.log(mealId);
     }
   }
-  ]);
+})();
