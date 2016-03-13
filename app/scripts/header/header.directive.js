ae7f59579add7a8312cd8bde7e3368df297f4bc7
Francesco Fiori <francesco.fiori@gmail.com>
2016-03-14 00:52:30 +0100

    fix add new item to cart

--- app/scripts/header/header.directive.js
+++ app/scripts/header/header.directive.js
@@ -9,9 +9,25 @@
     function appHeader () {
         var directive = {
             restrict: 'EA',
-            templateUrl: '/scripts/header/header.template.html'
+            templateUrl: '/scripts/header/header.template.html',
+            controller: HeaderCtrl,
+            controllerAs: 'headerC',
+            bindToController: true // because the scope is isolated
         };
 
         return directive;
+    }
+
+    HeaderCtrl.$inject = ['CartService'];
+    /* @ngInject */
+    function HeaderCtrl(CartService) {
+        var vm = this;
+        console.log('ciao');
+
+        vm.addToCart = addToCart;
+
+        function addToCart (meal) {
+            CartService.addToCart(meal);
+        }
     }
 })();
