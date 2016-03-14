92b1ca690b733372bacc2555f176ab22fc4ba923
Francesco Fiori <francesco.fiori@gmail.com>
2016-03-14 14:20:33 +0100

    fixed bug on main controller test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -8,7 +8,6 @@ describe('Controller: MainCtrl', function () {
     var MainCtrl,
         scope,
         MenuService,
-        CartService,
         controller;
 
     // Initialize the controller and a mock scope
@@ -16,23 +15,15 @@ describe('Controller: MainCtrl', function () {
         scope = $rootScope.$new();
         controller = $controller;
         MenuService = $injector.get('MenuService');
-        CartService = $injector.get('CartService');
         var success = function(func) {
             return func({resultCount: 1});
         };
         spyOn(MenuService, 'get').and.returnValue({success: success});
-        spyOn(CartService, 'getCartFromCookie').and.returnValue({success: success});
     }));
 
     it('should call the menu service to retrieve a list of meals', function () {
         var vm = controller("MainCtrl", { $scope: scope });
         expect(MenuService.get).toHaveBeenCalled();
         expect(vm.menu.resultCount).toBe(1);
-    });
-
-    it('should call the cart service to retrieve the cart list', function () {
-        var vm = controller("MainCtrl", { $scope: scope });
-        expect(CartService.getCartFromCookie).toHaveBeenCalled();
-        expect(vm.cart.items.length).toBe(0);
     });
 });
